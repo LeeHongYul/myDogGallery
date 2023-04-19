ac69385afb0eff2f0dc715d68e5f668e4c971478
이홍렬 <117960228+LeeHongYul@users.noreply.github.com>
2023-04-19 11:03:25 +0900

    Initial Commit

---
+++ myDogGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myDogGallery
+//
+//  Created by 이홍렬 on 2023/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
