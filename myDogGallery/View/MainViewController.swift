22ebef1812ca34921e1d2281329d196ecd25712e
이홍렬 <117960228+LeeHongYul@users.noreply.github.com>
2023-05-08 23:24:56 +0900

    Fix: 프로필 등록하면 MainView에 바로 프로필 관련된 페이지 컨트롤 안 나오는 문제 수정

--- myDogGallery/View/MainViewController.swift
+++ myDogGallery/View/MainViewController.swift
@@ -13,6 +13,7 @@ import SDWebImage
 
 class MainViewController: UIViewController {
     
+    @IBOutlet var mainProfileView: UIView!
     @IBOutlet var mainPageControl: UIPageControl!
     @IBOutlet var randomPhraseTextView: UITextView!
     @IBOutlet var weatherView: RoundedView!
@@ -27,13 +28,11 @@ class MainViewController: UIViewController {
     
     var weatherList = [Forcast.ForcastTemp]()
     
-    var airList = [Air.Response]()
-    
     var locationManager = CLLocationManager()
     
     @IBOutlet var mainCollectionView: UICollectionView!
     
-    @IBOutlet var testView: UIView!
+    @IBOutlet var mainGradientView: UIView!
     
     
     @IBAction func pageChaged(_ sender: UIPageControl) {
@@ -52,13 +51,17 @@ class MainViewController: UIViewController {
     }()
     
     override func viewDidLoad() {
-        super.viewDidLoad() 
+        super.viewDidLoad()
         
+        mainProfileViewSet()
+        
+        CoreDataManager.shared.fetchProfile()
+        print(CoreDataManager.shared.profileList.count,"프로필 수")
         mainPageControl.currentPage = 0
         mainPageControl.numberOfPages = CoreDataManager.shared.profileList.count
         
 
-        testView.setGradient(color1: UIColor.systemOrange, color2: UIColor.white)
+        mainGradientView.setGradient(color1: UIColor.systemOrange, color2: UIColor.white)
         
         fetchMoya()
         
@@ -78,6 +81,7 @@ class MainViewController: UIViewController {
             return section
             
         }
+        
         mainCollectionView.layer.borderWidth = 1
         mainCollectionView.layer.borderColor = UIColor.lightGray.cgColor
         mainCollectionView.layer.cornerRadius = 15
@@ -86,18 +90,35 @@ class MainViewController: UIViewController {
         
         setLocationManager()
         
-        //                fetchAirMoya()
+    }
+    
+    func mainProfileViewSet() {
+    
+        mainProfileView.layer.borderWidth = 1
+        mainProfileView.layer.borderColor = UIColor.lightGray.cgColor
+        mainProfileView.layer.cornerRadius = 15
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        if CoreDataManager.shared.profileList.count != 0 {
+            mainProfileView.isHidden = true
+        } else {
+            mainProfileView.isHidden = false
+        }
+        
+        CoreDataManager.shared.fetchProfile()
+        print(CoreDataManager.shared.profileList.count,"프로필 수22")
+        mainPageControl.currentPage = 0
+        mainPageControl.numberOfPages = CoreDataManager.shared.profileList.count
+        
+        
         CoreDataManager.shared.fetchMemo()
         if let inputeDate = CoreDataManager.shared.memoList.first?.inputDate {
             lastWalkDateLabel.text =  dateFormatter.string(from: inputeDate)
         }
         
         randomPhraseTextView.text = "\(phraselist[Int.random(in: 0 ... phraselist.count - 1)])"
-        
-        CoreDataManager.shared.fetchProfile()
+ 
         mainCollectionView.reloadData()
     }
     
@@ -116,36 +137,6 @@ class MainViewController: UIViewController {
             }
         }
     }
-    
-    
-    //        func fetchAirMoya() {
-    //            let provider = MoyaProvider<AirDataApi>()
-    //            provider.request(.airDataList) { result in
-    //                switch result {
-    //                case let .success(moyaResponse):
-    //                    let data = moyaResponse.data
-    //                    let statusCode = moyaResponse.statusCode
-    //                    print(moyaResponse.statusCode)
-    //
-    //                    do {
-    //                        try moyaResponse.filterSuccessfulStatusCodes()
-    //
-    //                        let decoder = JSONDecoder()
-    //                        let list = try decoder.decode(Air.self, from: data)
-    //
-    //                        self.airList = [list.response]
-    //                        DispatchQueue.main.async {
-    //                            print(list.response.body.items[0].informOverall)
-    //                        }
-    //                    } catch {
-    //                        print(error)
-    //                    }
-    //
-    //                case let .failure(error):
-    //                    break
-    //                }
-    //            }
-    //        }
     
     func fetchMoya() {
         let provider = MoyaProvider<WeatherDataApi>()
@@ -169,11 +160,11 @@ class MainViewController: UIViewController {
                         self.weatherTempLabel.text = tempStr+"°"
                         
                         let urlStr = "https://openweathermap.org/img/wn/" + (list.weather[0].icon)! + "@2x.png"
-                        print(urlStr)
+                        
                         switch list.weather[0].icon {
                         case "01n","01d":
                             self.mainWeatherImageView.image = UIImage(named: "sun")
-                        case "02n","03n", "04n","05n","02d","03d", "04d","05d":
+                        case "02n","03n", "04n","05n","02d","03d", "04d","05d","50n","50d":
                             self.mainWeatherImageView.image = UIImage(named: "fog")
                         case "09n", "10n","11n","09d", "10d","11d":
                             self.mainWeatherImageView.image = UIImage(named: "rain")
@@ -206,7 +197,7 @@ extension UIView{
         gradient.endPoint = CGPoint(x: 5.0, y: 1.0)
         gradient.frame = bounds
         gradient.type = .axial
-        gradient.cornerRadius = 15
+//        gradient.cornerRadius = 15
         layer.addSublayer(gradient)
     }
 }
@@ -216,7 +207,6 @@ extension MainViewController: UICollectionViewDataSource {
         return 1
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(CoreDataManager.shared.profileList.count)
         return CoreDataManager.shared.profileList.count
     }
     
@@ -235,7 +225,6 @@ extension MainViewController: UICollectionViewDataSource {
 
 extension MainViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        
         mainPageControl.currentPage = indexPath.row
     }
 }
